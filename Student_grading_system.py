4214dd39b1f1daabd10878e786053403d064072e
kishorethathamsetty33-bit <kishorethathamsetty33@gmail.com>
2026-01-20 17:58:02 +0530

    Create Student_grading_system.py
    students marks program

---
+++ Student_grading_system.py
@@ -0,0 +1,14 @@
+marks = int(input("enter your marks: "))
+if marks >= 90:
+     grade = "A+"
+elif marks >= 80:
+      grade == "A"
+elif marks >= 70:
+      grade == "B"
+elif marks >==60
+      grade == "c"
+elif marks >==40
+       grade == "pass"
+else:
+    grade = "Fail"
+print("Your Grade Is:", grade)
