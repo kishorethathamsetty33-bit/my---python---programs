e407de32cfc7570b6e52cb5d39ce0019c4b7a2d3
kishore <kishorethathamsetty33@gmail.com>
2026-05-26 19:35:50 +0530

    restored files

---
+++ bill_discount.py
@@ -0,0 +1,17 @@
+ampunt = int(input("enter a  total bill amount: "))
+items  = int(input(enter the number of items: "))
+discount = 0
+if items == 1:
+    discount == 10:
+elif items == 2:
+    discount = 20
+elif items >=3:
+    discount = 30
+discount_amount = amount*ndiscount / 100
+final_amount = amount-discount_amount
+print("original amount:", amount)
+print("discount", discount, %)
+print("you saved amount:", discount_amount)
+print("final_amount to pay:", final_amount)
+
+                   
---
+++ prime.py
@@ -0,0 +1,13 @@
+n = 5
+flag = True
+if n <= 1:
+    flag = False
+else:
+    for i in range(2, n):
+        if n % i == 0:
+            flag = False
+            break
+if flag:
+    print("Prime")
+else:
+    print("Not Prime")
